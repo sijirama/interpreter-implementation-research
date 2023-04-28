a260b71cce844ea566be4ba3931173c94fd17971
sijibomi <gbemilesanmi@gmail.com>
2023-04-28 23:49:08 +0100

    chore: init project and env

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,11 @@
 import Express from "express"
 import { env } from "./config/environment"
 const app = Express()
-const PORT = process.env.PORT || 5173
+//const PORT = process.env.PORT || 5173
 
 
-console.log(env.Hello!)
 
 
-app.listen(PORT , () => {
-    console.log("Server is now listening at port:",PORT)
+app.listen( env.PORT , () => {
+    console.log("Server is now listening at port:", env.PORT)
 })
--- src/config/environment.ts
+++ src/config/environment.ts
@@ -3,5 +3,6 @@ dotenv.config()
 
 export const env = {
     Hello:process.env.HELLO,
+    PORT: process.env.PORT || 5174
 }
 
