4dd130e6c4a718ed5e3e4ddbb9bc5b9fd06b979a
sijibomi <gbemilesanmi@gmail.com>
2023-04-29 16:24:25 +0100

    feat: user controller(getAllUsers)

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import Express from "express"
 import { env } from "./config/environment"
 import mongoose from "mongoose"
 import { connectDatabase } from "./config/connectDB"
+import { router as UserRouter } from "./routes/user.routes"
 connectDatabase()
 const app = Express()
 
 
+app.use("/api/user" , UserRouter)
 
 mongoose.connection.once("open" , () => {
     app.listen( env.PORT , () => {
