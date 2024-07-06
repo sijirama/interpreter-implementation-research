e6003ee7977b55987c208a1adff6af034e3ad4b9
sijirama <gbemilesanmi@gmail.com>
2024-07-06 02:07:11 +0100

    lox

--- lox/scanner/scanner.cpp
+++ lox/scanner/scanner.cpp
@@ -1,10 +1,72 @@
 #include "scanner.h"
+#include "../error/error.h"
 
 using namespace std;
+void Scanner::scanToken() {
+
+    char c = advance();
+
+    switch (c) {
+    case '(':
+        addToken(TokenType::LEFT_PAREN);
+        break;
+    case ')':
+        addToken(TokenType::RIGHT_PAREN);
+        break;
+    case '{':
+        addToken(TokenType::LEFT_BRACE);
+        break;
+    case '}':
+        addToken(TokenType::RIGHT_BRACE);
+        break;
+    case ',':
+        addToken(TokenType::COMMA);
+        break;
+    case '.':
+        addToken(TokenType::DOT);
+        break;
+    case '-':
+        addToken(TokenType::MINUS);
+        break;
+    case '+':
+        addToken(TokenType::PLUS);
+        break;
+    case ';':
+        addToken(TokenType::SEMICOLON);
+        break;
+    case '*':
+        addToken(TokenType::STAR);
+        break;
+    default:
+        error(line, "Unexpected character");
+        break;
+    }
+}
 
 Scanner::Scanner(string source) : source(source) {}
 
+
+bool Scanner::isAtEnd() {
+    bool ans = current >= static_cast<int>(source.length());
+    return ans;
+}
+
+void Scanner::addToken(TokenType type) { addToken(type, ""); }
+
+void Scanner::addToken(TokenType type, string literal) {
+    string text = source.substr(start, current);
+    tokens.push_back(Token(type, text, literal, line));
+}
+
+char Scanner::advance() {
+    current++;
+    return source[current - 1];
+}
+
 vector<Token> Scanner::scanTokens() {
-    vector<Token> tokens;
+    while (!isAtEnd()) {
+        start = current;
+    };
+    tokens.push_back(Token(TokenType::END_OF_FILE, "", "", line));
     return tokens;
 }
--- lox/scanner/scanner.h
+++ lox/scanner/scanner.h
@@ -6,8 +6,17 @@
 class Scanner {
   private:
     std::string source;
+    std::vector<Token> tokens;
+    int start = 0;
+    int current = 0;
+    int line = 1;
 
   public:
     Scanner(std::string source);
     std::vector<Token> scanTokens();
+    bool isAtEnd();
+    void scanToken();
+    char advance();
+    void addToken(TokenType type);
+    void addToken(TokenType type, std::string literal);
 };
